081972405b3189a6a128b2596e0d96df63b5b7db
tuantuan <lisuji1986@gmail.com>
2017-04-05 21:32:56 +0800

    ajax delete create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,13 @@ class PostsController < ApplicationController
      @post.user = current_user
      @post.save
 
-     redirect_to posts_path
    end
 
    def destroy
      @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
      @post.destroy
 
-     redirect_to posts_path
+    # render :js => "alert('ok');"
    end
 
    protected
